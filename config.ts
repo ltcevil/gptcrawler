1c632c9506f62ef8ea8624c75d37a4820259205c
ltcevil <84654599+ltcevil@users.noreply.github.com>
2024-06-08 03:15:21 +0200

    Update config.ts

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ import { Config } from "./src/config";
 export const defaultConfig: Config = {
   url: "https://www.builder.io/c/docs/developers",
   match: "https://www.builder.io/c/docs/**",
-  maxPagesToCrawl: 50,
-  outputFileName: "output.json",
+  maxPagesToCrawl: 5000,
+  outputFileName: "./data/output.json",
   maxTokens: 2000000,
 };
